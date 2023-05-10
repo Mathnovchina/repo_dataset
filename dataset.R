63fa0982b297983c19fc3a8008a36ac7f103d3eb
Mathnovchina <matthieu.bordenave@etu.utc.fr>
2023-05-10 12:27:33 +0200

    deflator 2010 2015

--- dataset.R
+++ dataset.R
@@ -34,6 +34,14 @@ filters1 = list( cofog99= "Total",
 data_exp <- get_eurostat("GOV_10A_EXP", filters = filters1)
 data_exp = rename(data_exp,exp_tot = values)
 
+# Government final consumption  (has to be deflated, current millions euros)
+
+filter_c = list(na_item = "P3_S13",
+                 unit = "CP_MEUR")
+data_cons <- get_eurostat("TEC00010", filters = filter_c)
+data_cons = rename(data_cons,pub_cons = values)
+
+# above not good, only 12 years
 
 # National expenditure on environmental protection, total economy, millions euro
 
@@ -46,27 +54,59 @@ data_join <-merge(data_exp,data_env, by = c("geo", "time"), all = TRUE)
 data_join = rename(data_join,exp_env=values.y)
 data_join$time = format(as.Date(data_join$time, format="%d/%m/%Y"),"%Y")
 
+env_exp = "EPER"
+envstruc <- get_data_structure(env_exp)   # list of dataframes with human-readable values for variable names and values
+str(envstruc, max.level = 1)
+envstruc$VAR_DESC
+envstruc$TABLES
+envstruc$MEASURE
+envstruc$EXP
+env_expdat <- get_dataset(env_exp,filter = list("INV","PUB","NATCURR")) # millions national currency 
+
+
 # GDP 
 
 gdp_base <- wb_search("unemployment")
 
-# Unemployment 
+## Unemployment 
 oecddataset_list <- get_datasets()
 
 dataset_list <- get_datasets()
-search_dataset("unemployment", data = dataset_list)
+  # Thousand person working 
+
+data_employ <- get_eurostat("NAMA_10_A10_E", filters = list(na_item = "EMP_DC", nace_r2 = "TOTAL", unit = "THS_PER"))
+colnames(data_employ)[colnames(data_employ) == 'values'] <- 'ths_wpeople'
+data_employ$time = format(as.Date(data_employ$time, format ="%d/%m/%Y"),"%Y")
+emp <- search_dataset("work", data =dataset_list)
+
+
+  # thousand hour worker 
+data_hour <- get_eurostat("NAMA_10_A10_E", filters = list(na_item = "EMP_DC", nace_r2 = "TOTAL", unit = "THS_WH"))
+colnames(data_hour)[colnames(data_hour) == 'values'] <- 'ths_whour'
+data_hour$time = format(as.Date(data_hour$time, format ="%d/%m/%Y"),"%Y")
+
 
 df2 <- "MIG_NUP_RATES_GENDER"
 
-dstruc <- get_data_structure(df2)   # list of dataframes with human-readable values for variable names and values
-str(dstruc, max.level = 1)
+emp <- "GENDER_EMP"
+
+dstruc4 <- get_data_structure(emp)   # list of dataframes with human-readable values for variable names and values
+str(dstruc4, max.level = 1)
 dstruc$VAR_DESC
 dstruc$RATE
 dstruc$COUNTRY
 dstruc$GENDER
+dstruc$UNIT
+
+dstruc4$VAR_DESC
+dstruc4$IND
+dstruc$OBS_STATUS 
+dstruc$GENDER
+dstruc4$UNIT
 
 filter_list <- "U_RATE"
 df <- get_dataset(dataset = df2, RATE = "U_RATE")
+df4 <- get_dataset(dataset = emp, IND = "EMP15_T")
 df_emp = subset(df, RATE == "U_RATE")
 df_emptot = subset(df_emp, GENDER == "TOT")
 colnames(df_emp)[colnames(df_emp) == 'COUNTRY'] <- 'geo'
@@ -118,7 +158,7 @@ sapply(data_join,class)
 # growth rate / GDP
 
 
-search_dataset("GDP", data = dataset_list)
+pub <- search_dataset("expenditure", data = dataset_list)
 dataset = "SNA_TABLE1" # Gross domestic Product data base 
 
 dstruc2 <- get_data_structure(dataset)   # list of dataframes with human-readable values for variable names and values
@@ -140,10 +180,12 @@ filter_gdp <- c( MEASURE = )
 data_gdp <- get_eurostat("nama_10_gdp", filters = list(geo = countries, na_item = "B1GQ", unit = "CP_MEUR"))
 data_gdp$time = format(as.Date(data_gdp$time, format="%d/%m/%Y"),"%Y")
 
-# Price index : implicit deflator 2010 = 100 euros PD10EUR
+# Price index : implicit deflator 2010 = 100 euros PD10EUR 
+data_gdp_deflator10 <- get_eurostat("nama_10_gdp", filters = list(geo = countries, na_item = "B1GQ", unit = "PD110_EUR"))
+data_gdp_deflator10$time = format(as.Date(data_gdp_deflator10$time, format="%d/%m/%Y"),"%Y")
 
-data_gdp_deflator <- get_eurostat("nama_10_gdp", filters = list(geo = countries, na_item = "B1GQ", unit = "PD10_EUR"))
-data_gdp_deflator$time = format(as.Date(data_gdp_deflator$time, format="%d/%m/%Y"),"%Y")
+data_gdp_deflator15 <- get_eurostat("nama_10_gdp", filters = list(geo = countries, na_item = "B1GQ", unit = "PD15_EUR"))
+data_gdp_deflator15$time = format(as.Date(data_gdp_deflator15$time, format="%d/%m/%Y"),"%Y")
 
 
 # CHAIN Link Volue ? 
